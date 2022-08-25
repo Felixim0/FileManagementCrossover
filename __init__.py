962cb46029546de2f4779afa2350f21bc1b4371b
Felixim0 <aldam.gatesfelix@gmail.com>
2022-08-25 22:48:44 +0100

    Finshed up, all functions work

--- __init__.py
+++ __init__.py
@@ -6,20 +6,28 @@ import webbrowser
 import os
 
 googleDriveDirectory  = ''
+downloadDirectory = ''
 
 def getUserWindow():
     global googleDriveDirectory
     googleDriveDirectory = filedialog.askdirectory()
 
+def getDownloadPath():
+    global downloadDirectory
+    downloadDirectory = filedialog.askdirectory()
+
 def runCMD(cmd):
     #os.system(f'{cmd}')
-    os.system('"X:\My Drive\Felix\Test\TESSST.gdoc"')
+    #os.system('"X:\My Drive\Felix\Test\TESSST.gdoc"')
+    print(cmd)
+    os.system(f'{cmd}')
 
 def openURL(url):
     webbrowser.open('http://example.com')
 
 def getAllPaths():
     global googleDriveDirectory
+    global paths
     print('Beginnging')
     if googleDriveDirectory == '':
         print('No src Directory provided')
@@ -28,9 +36,13 @@ def getAllPaths():
     for path in Path(googleDriveDirectory).rglob('*.gdoc'):
         paths.append(path)
 
-    for p in paths:
-        print(p)
     return paths
+
+def openAllGdocs():
+    global paths
+    for p in paths:
+        cmd = '"'+ str(p) + '"'
+        runCMD(cmd)
 
 class mainWindow(object):
     def __init__(self,master):
@@ -39,6 +51,11 @@ class mainWindow(object):
         self.b=Button(master,text="Select Start Directory",
             command=getUserWindow)
         self.b.pack()
+
+        self.ba=Button(master,text="Select Download Directory",
+            command=getDownloadPath)
+        self.ba.pack()
+
         self.b2=Button(master,text="Print to Check Working Directory",
             command=lambda: print(googleDriveDirectory))
         self.b2.pack()
@@ -48,7 +65,7 @@ class mainWindow(object):
         self.b3.pack()
 
         self.b4=Button(master,text="TEST",
-            command= lambda: runCMD('\"example.com\"'))
+            command= openAllGdocs)
         self.b4.pack()
 
 if __name__ == "__main__":
